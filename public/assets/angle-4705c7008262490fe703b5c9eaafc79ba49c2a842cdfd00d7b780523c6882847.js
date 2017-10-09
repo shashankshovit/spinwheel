8760106b748b45064bf89ecf40136145f04b63b1
Shashank Shovit <shashankshovit@gmail.com>
2017-10-10 04:06:56 +0530

    precompile

---
+++ public/assets/angle-4705c7008262490fe703b5c9eaafc79ba49c2a842cdfd00d7b780523c6882847.js
@@ -0,0 +1,22 @@
+class Angle {
+	constructor(angle){
+		this.angle = angle;
+	}
+
+	setAngle(angle){
+		this.angle = angle;
+	}
+
+	getAngle(){
+		return this.angle;
+	}
+
+	value(){
+		return this.angle;
+	}
+
+	toRad(){
+		return this.angle*Math.PI/180;
+	}
+}
+;
